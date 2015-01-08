28ff031e368d694962f25daeb336f432022431d9
EC2 Default User <ec2-user@ip-10-230-33-40.ec2.internal>
2015-01-08 19:17:37 +0000

    removing files not able to be run with locally-provided data

--- single-machine/public-commoncrawl.sh
+++
@@ -1,8 +0,0 @@
-ess instance local
-ess datastore select s3://aws-publicdatasets/common-crawl/crawl-data/CC-MAIN-2013-20/ --credentials=/home/ec2-user/jobs/asi-public.csv
-ess datastore scan
-ess datastore summary
-ess datastore ls "*" | wc -l
-ess datastore ls "*" | head -10
-echo "broken pipe error message is expected since the ls command is still reading data when the head -10 finishes"
-# can also run on CC-MAIN-2014-35 if using m3.medium
