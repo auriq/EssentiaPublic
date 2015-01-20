0a69dad37d1d6d42e24ababec6da6cc52dc50f3a
EC2 Default User <ec2-user@ip-10-0-0-85.ec2.internal>
2015-01-20 00:57:46 +0000

    much more versatile integrater.R, stream multiple files and use multiple lines per statement

--- single-machine/integrater.R
+++ single-machine/integrater.R
@@ -5,20 +5,59 @@
 file <- "rintapache.sh"
 rscriptfile <- "rintapache.R"
 
-commandcount <- 0
+commandcount <- 1
+lineold <- ""
 lines <- readLines(file)
 for (line in lines) {
-  #print(line)
+#  print(line)
+  line <- paste(lineold,line,sep="")
+  if (substr(line,nchar(line),nchar(line)) == "\\") {
+       lineold <- substr(line,1,nchar(line)-1)
+       next
+  }
   if (((substr(line, 1, 13) == "ess task exec") && (substr(line, nchar(line) - 7, nchar(line)) != "#Rignore")) || ((substr(line, 1, 15) == "ess task stream") && (substr(line, nchar(line) - 8, nchar(line)) == "#Rinclude"))) {
-    commandcount <- commandcount + 1
-    skiplines <- 0
-    if ((substr(line, 1, 15) == "ess task stream") && (substr(line, nchar(line) - 8, nchar(line)) == "#Rinclude")) {
-      skiplines <- 1
+    colspec <- TRUE
+    if (grepl("-notitle", line)) {
+       colspec <- FALSE
     }
     t1 <- pipe(line,open="r")
-    t2 <- read.csv(t1,header=TRUE,sep=",",quote="\"'",comment.char = "#",blank.lines.skip=FALSE,allowEscapes=TRUE,skip=skiplines)
-    assign(sprintf("command%i",commandcount), t2[1:(which(t2[,1] == 'RSTOPHERE')[[1]] - 1),1:ncol(t2)])
-    #print(get(sprintf("command%i",commandcount)))
+    t2 <- read.csv(t1,header=colspec,sep=",",quote="\"'",comment.char = "#",blank.lines.skip=FALSE,allowEscapes=TRUE,skip=0)
+    index <- 1
+    t3 <- NULL
+    separate <- grepl(" #Rseparate", line)
+    for (file in seq(1,length(which(t2[,1] == 'RSTOPHERE')),1)) {
+        if (separate) {
+           assign(sprintf("command%i",commandcount), t2[index:(which(t2[,1] == 'RSTOPHERE')[[file]] - 1),1:ncol(t2)])
+           index <- which(t2[,1] == 'RSTOPHERE')[[file]] + 1
+           print(get(sprintf("command%i",commandcount)))
+#           print(commandcount)
+           commandcount <- commandcount + 1
+        }
+        else {
+           t3 <- rbind(t3, t2[index:(which(t2[,1] == 'RSTOPHERE')[[file]] - 1),1:ncol(t2)])
+           index <- which(t2[,1] == 'RSTOPHERE')[[file]] + 1
+           if (file == length(which(t2[,1] == 'RSTOPHERE'))) {
+              assign(sprintf("command%i",commandcount), t3)
+              print(get(sprintf("command%i",commandcount)))
+              print(sprintf("---------------- Output Stored in command%i ----------------", commandcount))
+              commandcount <- commandcount + 1
+           }
+        }
+    }
+    if ((file > 1) && (grepl(" #Rseparate", line))) {
+        print(sprintf("---------------- Stream Completed: %i files stored in %i commands: command%i to command%i ----------------",file, file, commandcount - file, commandcount - 1))
+        close(t1)
+        remove(t1)
+        remove(t2)
+        linepart1 <- unlist(strsplit(line,split=" "))
+        t1 <- pipe(paste(linepart1[[1]],linepart1[[2]],linepart1[[3]],linepart1[[4]],linepart1[[5]],linepart1[[6]],"\"aq_pp -f,eok - -d %cols 2> /dev/null | echo %file \"", sep=" "),open="r")
+        t2 <- read.csv(t1,header=FALSE,sep=",",quote="\"'",comment.char = "#",blank.lines.skip=FALSE,allowEscapes=TRUE,skip=0)
+        assign(sprintf("command%i",commandcount), t2[1:file,1:ncol(t2)])
+        print(get(sprintf("command%i",commandcount)))
+#        print(commandcount)
+        print(sprintf("---------------- Filenames stored in command%i ----------------", commandcount))
+        commandcount <- commandcount + 1
+    }
     close(t1)
     remove(t1)
     remove(t2)
@@ -29,6 +68,8 @@ for (line in lines) {
     }
   }
 }
+
+print(sprintf("---------------- There are a total of %i commands ----------------", commandcount - 1))
 
 source(rscriptfile, echo=TRUE)
 # Turn echo to FALSE to make the output on your screen more results-oriented
